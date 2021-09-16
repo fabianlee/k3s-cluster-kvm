99937ca5bf9675c4dcfe89f9ce76494fa8f39cbd
flee999 <flee999@hotmail.com>
2021-09-15 23:02:05 -0400

    more robust check for URL domains

--- test-ngingx-endpoints.sh
+++ test-ngingx-endpoints.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+for h in k3s.local k3s-secondary.local; do
+  nslookup -timeout=1 $h
+  if [ $? -ne 0 ]; then
+    echo "WARN are you sure $h is DNS resolvable? did you add it to /etc/hosts?"
+    #exit 3
+  fi
+done
+
 curlopt="-k --fail --connect-timeout 3 --retry 0"
 while [ 1==1 ]; do 
   timestamp=$(date +"%D %T")
