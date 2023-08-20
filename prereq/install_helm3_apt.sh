bee84de6c61e507e23a8997cb7c1c07a75c30ab8
Fabian Lee <flee999@hotmail.com>
2023-08-20 11:40:31 -0400

    adding hpa for hello deployment

--- prereq/install_helm3_apt.sh
+++ prereq/install_helm3_apt.sh
@@ -3,10 +3,11 @@
 
 set -x
 
-curl https://baltocdn.com/helm/signing.asc | sudo apt-key add -
-sudo apt-get install apt-transport-https --yes
-echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
-sudo apt-get update
-sudo apt-get install helm
+curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
+sudo chmod 644 /usr/share/keyrings/helm.gpg
+sudo apt install apt-transport-https --yes
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+sudo apt update
+sudo apt install helm -y
 
 helm version
